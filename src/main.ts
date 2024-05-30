12adfe4c727f1a09ec61fe2d2265953674476e3c
Joe Reidell <joe@reidell.net>
2024-05-30 00:01:14 -0400

    implemented hbs partials to provide reusable handlebar chunks. added hbsUtils, a main layout template and a bootstrap css reference to the layout header.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
@@ -7,6 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+  hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
   app.setViewEngine('hbs');
   await app.listen(3000);
 }
